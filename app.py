bd853bd99d9be7a4994548c5bdc2b5ee4fee18eb
Ubuntu <ubuntu@ubuntu16.u0rrfxxukazezmnmyhiw4qtnof.hx.internal.cloudapp.net>
2022-11-29 22:19:48 +0000

    update: credentials

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask_cors import CORS
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
+from flask_cors import cross_origin
 
 
 
 app = Flask(__name__)
-CORS(app, resources=r'/*')
-
+#CORS(app, resources=r'/*')
+CORS(app, resources={r"/.*": {"origins": ["http://127.0.0.1","https://yuenci.github.io/"]}}, supports_credentials=True) 
+#CORS(app, supports_credentials=True)
 
 @app.route('/data', methods=["POST"])
 def postExplainToCache():
@@ -15,6 +17,14 @@ def postExplainToCache():
     response ="[[[[[" + sentence + "]]]]]" 
     return{"response": response}
 
+
+@app.after_request
+def after(resp):
+    resp = make_response(resp)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return resp
 
 
 if __name__ == '__main__':
